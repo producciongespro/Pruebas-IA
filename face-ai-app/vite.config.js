f2aba131aaf25ceeefa127e44b14036ae5557cc2
lchaconc <luis.chacon.campos@gmail.com>
2024-12-17 13:00:11 -0600

    Se elimina proyecto face-api -debido a que usa librerías obsoletas -Es una librería que no se le ha dado mantenimiento en 5 años

--- face-ai-app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: "./",
-  build: {
-    assetsDir: "./", // Mantén los archivos en la raíz de la carpeta `dist`
-  },
-});
